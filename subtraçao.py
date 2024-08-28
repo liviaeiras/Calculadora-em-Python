4f1151ba4542e1d3aafede5aa3522c3748fb365e
Lívia Eiras <livyeiras@gmail.com>
2024-08-28 01:52:36 -0300

    Add files via upload
    Calculadora em linguagem python que realiza apenas operações: subtração.

---
+++ subtraçao.py
@@ -0,0 +1,9 @@
+# Solicita ao usuário para digitar os números
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+# Realiza a subtração
+resultado = num1 - num2
+
+# Exibe o resultado
+print("Resultado:", resultado)
